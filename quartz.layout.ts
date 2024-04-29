ec2cb3c4a0285b0ea7145313f16a97995830c207
xyang <121338860+xyang8@users.noreply.github.com>
2024-04-29 19:56:26 +0200

    Quartz sync: Apr 29, 2024, 7:56 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,6 +5,8 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
+  footer: Component.Footer({
+  }),
 }
 
 // components for pages that display a single page (e.g. a single note)
